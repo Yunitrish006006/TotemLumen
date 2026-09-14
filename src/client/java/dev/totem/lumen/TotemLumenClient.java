60ab7ee86e63daad32ffe5cb735a5fdf3a9e8c47
阿昀 <yunthomas006@gmail.com>
2026-09-14 22:53:28 +0800

    feat: display P5 Vulkan debug texture on HUD

--- src/client/java/dev/totem/lumen/TotemLumenClient.java
+++ src/client/java/dev/totem/lumen/TotemLumenClient.java
@@ -4,10 +4,13 @@ import dev.totem.lumen.integration.SceneExtractionBridge;
 import dev.totem.lumen.render.RendererBootstrap;
 import dev.totem.lumen.vulkan.P4ComputeSmokeTest;
 import dev.totem.lumen.vulkan.P4DdaSmokeTest;
+import dev.totem.lumen.vulkan.P5DebugCompositeTest;
 import dev.totem.lumen.vulkan.P5DebugRayGridTest;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.rendering.v1.hud.HudElementRegistry;
 import net.fabricmc.fabric.api.client.rendering.v1.level.LevelRenderEvents;
+import net.minecraft.resources.Identifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -30,6 +33,12 @@ public final class TotemLumenClient implements ClientModInitializer {
             P4ComputeSmokeTest.runOnceOnRenderThread();
             P4DdaSmokeTest.runOnceOnRenderThread();
             P5DebugRayGridTest.runOnceOnRenderThread();
+            P5DebugCompositeTest.runOnceOnRenderThread();
         });
+
+        HudElementRegistry.addLast(
+                Identifier.fromNamespaceAndPath(MOD_ID, "p5_debug_overlay"),
+                (graphics, deltaTracker) -> P5DebugCompositeTest.drawHud(graphics)
+        );
     }
 }
