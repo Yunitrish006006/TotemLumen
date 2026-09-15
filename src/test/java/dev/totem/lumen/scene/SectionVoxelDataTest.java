e9045f8fa055c8b29f0493cb50d4886cb8fc6cd7
阿昀 <yunthomas006@gmail.com>
2026-09-15 20:16:54 +0800

    test: cover P14 packed voxel geometry ABI

--- src/test/java/dev/totem/lumen/scene/SectionVoxelDataTest.java
+++ src/test/java/dev/totem/lumen/scene/SectionVoxelDataTest.java
@@ -24,6 +24,28 @@ class SectionVoxelDataTest {
         ids[SectionVoxelData.index(1, 2, 3)] = 7;
 
         assertEquals(42, data.materialId(1, 2, 3));
+        assertEquals(BlockGeometryCode.FULL_CUBE, data.geometryCode(1, 2, 3));
+    }
+
+    @Test
+    void packedVoxelWordPreservesMaterialAndGeometryIndependently() {
+        int word = SectionVoxelData.packVoxelWord(4095, BlockGeometryCode.SLAB_TOP);
+        int[] words = new int[SectionVoxelData.VOXEL_COUNT];
+        words[SectionVoxelData.index(4, 5, 6)] = word;
+        SectionVoxelData data = new SectionVoxelData(words);
+
+        assertEquals(4095, data.materialId(4, 5, 6));
+        assertEquals(BlockGeometryCode.SLAB_TOP, data.geometryCode(4, 5, 6));
+        assertEquals(4095, SectionVoxelData.unpackMaterialId(word));
+        assertEquals(BlockGeometryCode.SLAB_TOP, SectionVoxelData.unpackGeometryCode(word));
+        assertEquals(word, data.copyMaterialIds()[SectionVoxelData.index(4, 5, 6)]);
+        assertEquals(4095, data.copyRawMaterialIds()[SectionVoxelData.index(4, 5, 6)]);
+    }
+
+    @Test
+    void packedVoxelWordRejectsValuesOutsideSixteenBits() {
+        assertThrows(IllegalArgumentException.class, () -> SectionVoxelData.packVoxelWord(0x1_0000, 0));
+        assertThrows(IllegalArgumentException.class, () -> SectionVoxelData.packVoxelWord(1, 0x1_0000));
     }
 
     @Test
